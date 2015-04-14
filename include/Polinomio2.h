2f807a4d05e8c96a26f66b13a62057ab6bf8c260
Haylin Ellenes <haylin-chama@hotmail.es>
2015-04-14 14:27:18 -0300

    ashdkshajkdhasd

---
+++ include/Polinomio2.h
@@ -0,0 +1,13 @@
+#ifndef POLINOMIO2_H
+#define POLINOMIO2_H
+
+class Polinomio2
+{
+    public:
+        Polinomio2();
+        virtual ~Polinomio2();
+    protected:
+    private:
+};
+
+#endif // POLINOMIO2_H
---
+++ src/Polinomio2.cpp
@@ -0,0 +1,11 @@
+#include "Polinomio2.h"
+
+Polinomio2::Polinomio2()
+{
+    //ctor
+}
+
+Polinomio2::~Polinomio2()
+{
+    //dtor
+}
