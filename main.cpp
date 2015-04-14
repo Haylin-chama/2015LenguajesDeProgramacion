0a953eacc8089d5704e342b4d66f94256aabdccb
Haylin Ellenes <haylin-chama@hotmail.es>
2015-04-14 15:10:18 -0300

    djkashdjkadhas
    aaaaaaaaah

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "Polinomio2.h"
 
 using namespace std;
 
 typedef float POLINOMIO2[10][10];
+
+
 
 
 
@@ -11,8 +14,22 @@ void menu()
 
 }
 
+int CrearPolinomio()
+{
+    if (g>0)
+    {
+        cin>>c;
+        cin>>m;
+        cin>>n;
+        //Y se guarda la wea de C en la posición (m,n)
+        //:(
+            //Mesiento confundida
+    }
+}
+
 int main()
 {
+    int g;
     cout << "Hello world!" << endl;
-    return 0;
+    CrearPolinomio();
 }
